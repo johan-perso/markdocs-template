2a5b31d1f3a86ec30bfcabab47904c8aecf7605c
Johan <johan@johanstick.fr>
2025-12-04 17:58:16 +0100

    Update fumadocs + fix CVE-2025-55182

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { I18nProvider } from "fumadocs-ui/i18n";
+import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
@@ -12,19 +11,24 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <I18nProvider locale="fr" translations={{
-            toc: "Sur cette page",
-            search: "Rechercher",
-            lastUpdate: "Mis à jour le",
-            searchNoResult: "Aucun résultat trouvé",
-            previousPage: "Précédent",
-            nextPage: "Suivant",
-            chooseTheme: "Thème de l'interface",
-            tocNoHeadings: "Aucun titre sur la page",
-            editOnGithub: "Éditer sur GitHub",
-          }}>
-          <RootProvider>{children}</RootProvider>
-        </I18nProvider>
+        <RootProvider
+          i18n={{
+            locale: "fr",
+            translations: {
+              toc: "Sur cette page",
+              search: "Rechercher",
+              lastUpdate: "Mis à jour le",
+              searchNoResult: "Aucun résultat trouvé",
+              previousPage: "Précédent",
+              nextPage: "Suivant",
+              chooseTheme: "Thème de l'interface",
+              tocNoHeadings: "Aucun titre sur la page",
+              editOnGithub: "Éditer sur GitHub",
+            }
+          }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
